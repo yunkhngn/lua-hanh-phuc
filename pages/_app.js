c69357b187a3ad21e2fe508e7211f7f077a7261d
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-22 06:45:56 +0700

    Adding analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,13 @@ import { Navigate, NavBottom } from "@/components/template";
 import { styletron } from "../styletron";
 import { Provider as StyletronProvider } from "styletron-react";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }) {
   return (
     <StyletronProvider value={styletron}>
       <Component {...pageProps} />
+      <Analytics/>
       <Navigate />
       <NavBottom />
       <SpeedInsights/>
