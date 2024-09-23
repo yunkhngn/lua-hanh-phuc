9591fa9463716bad0883d4dff07b892713e49169
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-23 14:54:01 +0700

    Updating description

--- components/lib/desc.js
+++ components/lib/desc.js
@@ -3,37 +3,37 @@ const desc = {
         route: '/',
         title: 'Dự án "Sưởi ấm"',
         url: 'https://suoiam.vercel.app/',
-        description: 'Dự án sưởi ấm',
+        description: 'Dự án "sưởi ấm" là dự án từ thiện được thành lập bởi sinh viên trường đại học FPT.',
     },
     about: {
         route: '/about',
         title: 'Về chúng tớ',
         url: 'https://suoiam.vercel.app/about-us',
-        description: 'Giới thiệu về chúng tôi',
+        description: 'Giới thiệu tổng quan về dự án "Sưởi ấm".',
     },
     gallery: {
         route: '/gallery',
         title: 'Thư viện',
         url: 'https://suoiam.vercel.app/gallery',
-        description: 'Thư viện',
+        description: 'Thư viện ảnh của dự án "Sưởi ấm" và các hoạt động thiện nguyện.',
     },
     post: {
         route: '/post',
         title: 'Bài viết',
         url: 'https://suoiam.vercel.app/post',
-        description: 'Bài viết',
+        description: 'Bài viết về dự án "Sưởi ấm" và các hoạt động thiện nguyện.',
     },
     funding: {
         route: '/funding',
         title: 'Chiến dịch gây quỹ',
         url: 'https://suoiam.vercel.app/funding',
-        description: 'Chiến dịch gây quỹ',
+        description: 'Chiến dịch gây quỹ của dự án "Sưởi ấm".',
     }, 
     contact: {
         route: '/contact',
         title: 'Liên hệ',
         url: 'https://suoiam.vercel.app/contact',
-        description: 'Liên hệ với chúng tớ',
+        description: 'Liên hệ với chúng tớ để biết thêm thông tin về dự án "Sưởi ấm".',
     },
 }
 
