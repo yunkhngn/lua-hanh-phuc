7bef626dbe07da13e008f4dfe6e9c4cc8b6c9ac3
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-30 20:01:38 +0700

    Updating assets

--- components/template/Contact.js
+++ components/template/Contact.js
@@ -3,10 +3,29 @@ import {Div, Text} from 'atomize'
 const Contact = () => {
   return (
     <Div>
-        <Text>Liên hệ với chúng tớ để biết thêm thông tin về dự án Lửa hạnh phúc.</Text>
+        <Text
+        textSize="heading"
+        >Liên hệ với chúng tớ để biết thêm thông tin về dự án Lửa hạnh phúc.</Text>
         <br/>
-        <Text>Gmail: luahanhphuc@gmail.com</Text>
-        <Text>Facebook: Lửa Hạnh Phúc</Text>
+        <Text
+        textSize="subheader"
+        >
+        <i>
+        Dự án Lửa hạnh phúc được thành lập nên với mong muốn truyền tải và xây dựng các “Lớp học hạnh phúc” trong các cơ sở giáo dục và nhà trường.
+        </i>
+        </Text>
+        <Text
+        textSize="subheader"
+        >Trưởng dự án: Nguyễn Đăng Khoa</Text>
+        <Text
+        textSize="subheader"
+        >Giảng viên hướng dẫn: Khuất Thị Hoa</Text>
+        <Text
+        textSize="subheader"
+        >Gmail: luahanhphuc@gmail.com</Text>
+        <Text
+        textSize="subheader"
+        >Facebook: Lửa Hạnh Phúc</Text>
     </Div>
   )
 }
