a19c03a07c439ebf88109e41945e0ad7073b0bb3
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-30 20:24:00 +0700

    Updating assets

--- components/template/Home/Carousel.js
+++ components/template/Home/Carousel.js
@@ -7,7 +7,7 @@ const Carousel = () => {
     className="carousel"
     >
       <Image 
-      src="/stock/poster.png" 
+      src="/stock/poster.jpg" 
       alt="carousel1" 
       fill
       priority={true}
